18bcd6509f670b7b0ee9a09c8741b7fbe7193752
27917 <2791752775@qq.com>
2019-08-22 10:50:19 +0800

    git tracks change

--- src/com/acat/Main.java
+++ src/com/acat/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("git is a distributed version control system");
         System.out.println("git is a free software");
+        System.out.println("git has a mutable index called stage");
+        System.out.println("git tracks change");
     }
 }
