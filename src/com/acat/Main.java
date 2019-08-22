29ff44c807b9478ae4b8cb9d6d16312d6bb16852
27917 <2791752775@qq.com>
2019-08-22 10:18:32 +0800

    第二次提交，add distributed

--- src/com/acat/Main.java
+++ src/com/acat/Main.java
@@ -2,7 +2,7 @@ package com.acat;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("git is a version control system");
-        System.out.println("git is a software");
+        System.out.println("git is a distributed version control system");
+        System.out.println("git is a free software");
     }
 }
