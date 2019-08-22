f7b44e303074372ad61004a8636426b224123203
27917 <2791752775@qq.com>
2019-08-22 10:02:57 +0800

    第一次提交，write a readme file

---
+++ src/com/acat/Main.java
@@ -0,0 +1,8 @@
+package com.acat;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("git is a version control system");
+        System.out.println("git is a software");
+    }
+}
